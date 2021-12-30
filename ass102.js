a144900cf38f4bff6edcfd1405f9a64b20af94b4
Nikhil10062000 <94104092+Nikhil10062000@users.noreply.github.com>
2021-12-30 11:21:25 +0530

    Add files via upload

---
+++ ass102.js
@@ -0,0 +1,16 @@
+//write a program to have 3 variables and output the highest of all time
+
+
+var num1=1;
+    num2=2;
+    num3=3;
+  
+  if (num1 > num2 && num1 > num3)
+  { console.log("num1");
+    }
+    else if (num2 > num1 && num2 > num3)
+  { console.log("num2");
+    }
+    else {
+    console.log("num3");
+}
